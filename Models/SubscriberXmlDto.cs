ae61ef5d27a8aebcd611190c2b43cc0d50de35e8
Björn Nyman <bjorn.nyman@webflex.se>
2025-11-20 10:19:52 +0100

    Add dto foe xml

---
+++ Models/SubscriberXmlDto.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.Serialization;
+
+namespace Subscribersystem_API.Models
+{
+    [XmlType("Subscriber")]
+    public class SubscriberXmlDto
+    {
+        public string SubscriptionNumber { get; set; } = string.Empty;
+        public string PersonalNumber { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string PhoneNumber { get; set; } = string.Empty;
+        public string DeliveryAddress { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+    }
+
+    [XmlRoot("Subscribers")]
+    public class SubscriberXmlList
+    {
+        [XmlElement("Subscriber")]
+        public List<SubscriberXmlDto> Items { get; set; } = new();
+    }
+}
